854bb8e7b6c3eea84eb559ea4d282413148587c5
baoagile <ngoc.hb@agiletechsoftware.com>
2023-11-30 17:44:09 +0700

    check turn

--- prisma/migrations/20231130104357_check_turn/migration.sql
+++ prisma/migrations/20231130104357_check_turn/migration.sql
@@ -2,12 +2,11 @@
   Warnings:
 
   - You are about to drop the column `time` on the `health_check_appointment` table. All the data in the column will be lost.
-  - Added the required column `order` to the `health_check_appointment` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
 ALTER TABLE "health_check_appointment" DROP COLUMN "time",
-ADD COLUMN     "order" INTEGER NOT NULL,
+ADD COLUMN     "order" INTEGER NOT NULL DEFAULT -1,
 ALTER COLUMN "external_code" DROP NOT NULL,
 ALTER COLUMN "external_code" DROP DEFAULT,
 ALTER COLUMN "external_code" SET DATA TYPE TEXT;
