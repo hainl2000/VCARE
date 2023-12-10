1cb4013469be659a027f23dbb0608d520716b1ae
Hoang Ngoc Bao <79911907+letgobao@users.noreply.github.com>
2023-12-10 18:54:37 +0700

    Update health-status.service.ts

--- src/modules/health-status/health-status.service.ts
+++ src/modules/health-status/health-status.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/shared/prisma.service';
+import { Prisma, users } from '@prisma/client';
+import { getMeaningfulData } from 'src/utils/common';
+import { UpdateHeathStatusDto  } from './health-status.dto';
 
 @Injectable()
-export class HealthStatusService {}
+export class HealthStatusService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async update(data: UpdateHeathStatusDto, user: users) {
+      const currentStatus = user.health_status as Prisma.JsonObject;
+      const updateData = getMeaningfulData(data);
+      const health_status = { ... currentStatus, ...updateData };
+      const update = await this.prisma.users.update({
+        where: { id: user.id },
+        data: { health_status },
+      });
+
+      return this.getHealthStatus(update);
+    }
+
+    getHealthStatus(user: users) {
+      const result: Record<string, any> = {
+        height: null,
+        weight: null,
+        blood_type: null,
+        blood_pressure: null,
+      };
+
+      const currentStatus = user.health_status as Prisma.JsonObject;
+      Object.keys(result).forEach((key:string) => {
+        result[key] = currentStatus[key] ?? null;
+      });
+
+      return result;
+    }
+}
